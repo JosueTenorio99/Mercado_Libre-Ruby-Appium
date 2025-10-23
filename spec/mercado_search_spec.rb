7f106afd7d95405b88bb92136434012d47aaff98
JosueTenorio99 <tndjosue69@gmail.com>
2025-10-22 22:32:18 -0600

    Before Allure

--- spec/mercado_search_spec.rb
+++ spec/mercado_search_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe 'Mercado Libre Search Flow' do
     @results.save_SCREENSHOT
 
     @results.click_OPTION_NEW
+    @results.click_OPTION_NEW
     @results.save_SCREENSHOT
 
     @results.scroll_until_SORT_BY_BUTTON
